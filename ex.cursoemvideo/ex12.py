cc08e4a18a329f0233f402a33ea6b023dbb94490
clebera1 <clebergressoni80@gamil.com>
2023-07-16 21:45:20 -0300

    exercicio 12

---
+++ ex.cursoemvideo/ex12.py
@@ -0,0 +1,3 @@
+salario = float(input('Qual o salario do funcionario? R$'))
+reajuste = salario + salario * 0.15
+print(f'o funcionario que ganhava {salario} com reajuste de 15% agora passa a ganhar {reajuste:.2f}')
