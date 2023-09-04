5074b847056e2b2b0813bb06ddb02a2b2155c64f
clebera1 <clebergressoni80@gamil.com>
2023-09-04 00:42:49 -0300

    melhoria exercicio

--- ex.cursoemvideo/ex51.py
+++ ex.cursoemvideo/ex51.py
@@ -2,7 +2,7 @@ print('Os 10 primeiros termos de uma PA')
 
 primeiro_termo = int(input('Digite o primeiro termo: '))
 razao = int(input('Digite a razao: '))
-decimo_termo =  primeiro_termo + (10-1) * razao
+decimo_termo =  primeiro_termo + 9 * razao
 
 
 for c in range(primeiro_termo, decimo_termo + razao, razao):
